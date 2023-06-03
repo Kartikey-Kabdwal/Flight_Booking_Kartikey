6a7b861372314708abb963d585296d4364f6673d
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2023-06-03 20:57:36 +0530

    Add files via upload

---
+++ flights/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('search_flights/', views.search_flights, name='search_flights'),
+    path('book_flight/<int:flight_id>/', views.book_flight, name='book_flight'),
+    path('my_bookings/', views.my_bookings, name='my_bookings'),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
+    path('logout/', views.logout_view, name='logout'),
+]
---
+++ flights/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from .models import Flight, Booking
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+@login_required(login_url='/login/')
+def search_flights(request):
+    if request.method == 'POST':
+        departure_date = request.POST.get('departure_date')
+        flights = Flight.objects.filter(departure_date=departure_date)
+        return render(request, 'flight_list.html', {'flights': flights})
+    return render(request, 'search_flights.html')
+
+
+@login_required(login_url='/login/')
+def book_flight(request, flight_id):
+    flight = Flight.objects.get(id=flight_id)
+    if flight.available_seats > 0:
+        Booking.objects.create(user=request.user, flight=flight)
+        flight.available_seats -= 1
+        flight.save()
+        return redirect('my_bookings')
+    return render(request, 'flight_list.html', {'message': 'No seats available'})
+
+
+@login_required(login_url='/login/')
+def my_bookings(request):
+    bookings = Booking.objects.filter(user=request.user)
+    return render(request, 'my_bookings.html', {'bookings': bookings})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('search_flights')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
