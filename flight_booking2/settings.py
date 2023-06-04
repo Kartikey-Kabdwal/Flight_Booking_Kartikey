e8a9cd4d8badad4570e81ce5f72f125ff99e5fed
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2023-06-04 12:14:43 +0530

    Update settings.py

--- flight_booking2/settings.py
+++ flight_booking2/settings.py
@@ -55,9 +55,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-         'OPTIONS': {
-            'deterministic': False,  # Set to False to disable deterministic mode
-        },
+       
     }
 }
 ROOT_URLCONF = 'flight_booking2.urls'
